9e8aa95c2af868c6e76214c02bd229cb11bb9b31
Matheus Wilhelms de Souza <mws_c@outlook.com>
2022-09-11 03:39:45 -0400

    NotePad
    Finalizado com todas funções implementadas

--- Intro/JS/Ex05/main.js
+++ Intro/JS/Ex05/main.js
@@ -7,26 +7,60 @@ class NotePad{
 
     saveNote(){
         this.notes[this.currentNoteIndex] = this.textAreaElement.value;
-        console.log(this.notes[this.currentNoteIndex]);
+    }
+
+    deleteNote(){
+        if(this.notes.length <= 1)//Array of notes only has the note that is being deleted
+        {
+            this.notes[this.currentNoteIndex] = '';
+        }
+        else
+        {
+            this.notes.splice(this.currentNoteIndex, 1);
+
+            if(this.currentNoteIndex > 0)//Current note is NOT the first
+                this.currentNoteIndex--;
+            else                         //Current note is the first
+                this.currentNoteIndex++;
+        }
+
+        this.updateDisplay();
     }
 
     newNote(){
         this.saveNote();
-        this.notes.push('');
-        this.currentNoteIndex++;
-        this.updateDisplay();
+
+        if(this.textAreaElement.value !== '')
+        {
+            if(this.currentNoteIndex === this.notes.length - 1)
+                this.notes.push('');
+            else
+                this.notes.splice(this.currentNoteIndex + 1, 0, '');
+            
+            this.currentNoteIndex++;
+            this.updateDisplay();
+        }
     }
 
     navigate(buttonText){
+        this.saveNote();
+
         if(buttonText === "<<")
+        {
             if(this.currentNoteIndex > 0)
                 this.currentNoteIndex--;
+        }
+            
         else if(buttonText === ">>")
-            if(this.notes.length - 1 > this.currentNoteIndex)
+        {
+            if(this.currentNoteIndex < this.notes.length - 1)
                 this.currentNoteIndex++;
-
+        }  
+        
         this.updateDisplay();
+        console.log(this.currentNoteIndex);
     }
+
     updateDisplay(){
         this.textAreaElement.value = this.notes[this.currentNoteIndex];
     }
@@ -35,6 +69,7 @@ class NotePad{
 const textAreaElement = document.querySelector('[data-note]');
 const navigationButtons = document.querySelectorAll('[data-nav]');
 const saveButton = document.querySelector('[data-save]');
+const deleteButton = document.querySelector('[data-delete]');
 const newButton = document.querySelector('[data-new]');
 
 const notePad = new NotePad(textAreaElement);
@@ -47,6 +82,10 @@ navigationButtons.forEach(navButton => {
 
 saveButton.addEventListener("click", () => {
     notePad.saveNote();
+})
+
+deleteButton.addEventListener("click", () => {
+    notePad.deleteNote();
 })
 
 newButton.addEventListener("click", () => {
